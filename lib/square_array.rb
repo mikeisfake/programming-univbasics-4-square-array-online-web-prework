d44405cdc1d9e297fc17c5aa3a743c406eaa7b31
Mike Cooper <mikescooper@icloud.com>
2019-08-05 03:07:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,11 @@
 def square_array(array)
   counter = 0 
+  new_array = []
   
   while counter < array.length do
-    array[counter] ** 2 
+    new_el = array[counter] ** 2 
+    new_array.push(new_el)
     counter += 1 
-    return array 
+    return new_array 
   end
 end
